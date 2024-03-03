9d28caf5c3beaaa9793b311e05be3d6598f60e6b
austin2118ace <austin2118ace@gmail.com>
2024-03-03 15:22:03 -0500

    Extra documentation Code cleanup camelCase to snake_case

--- dewan_calcium/DewanPlotting.py
+++ dewan_calcium/DewanPlotting.py
@@ -1,22 +1,18 @@
-"""
-Created on Sun Dec  4 19:59:13 2022
-
-@author: A. Pauley, Dewan Lab
-"""
 import cycler
 import numpy as np
+import multiprocessing
+
+from functools import partial
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib.cm as cm
-from functools import partial
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-from multiprocessing import Pool
-from pathlib import Path
-from .helpers import DewanIOhandler
-from .helpers import DewanDataStore
-from .helpers import DewanTraceTools
 
-# noinspection DuplicatedCode
+from .helpers import DewanDataStore, DewanIOhandler, DewanTraceTools
+
+
 
 def generate_color_map(numColors: int):
     color_map = cm.get_cmap('rainbow')
@@ -24,69 +20,77 @@ def generate_color_map(numColors: int):
     return cycler.cycler('color', color_map(indices))
 
 
-def plot_cell_odor_traces(inputData: DewanDataStore.PlottingDataStore, latentCellsOnly: bool,
-                          plotAll: bool, cell: int) -> None:
+def truncate_data(data):
+    row_len = np.min([len(row) for row in data])
+    # print(row_len)
+    for i, row in enumerate(data):
+        data[i] = row[:row_len]
 
-    if latentCellsOnly:
+    return data
+
+
+def plot_cell_odor_traces(input_data: DewanDataStore.PlottingDataStore, latent_cells_only: bool,
+                          plot_all_cells: bool, cell_number: int) -> None:
+
+    if latent_cells_only:
         folder = 'LatentCells'
     else:
         folder = 'OnTimeCells'
 
     folders = ['AllCellTracePlots', folder]
 
-    inputData = inputData.makeCopy()
-    inputData.update_cell(cell)
-    cell_name = inputData.current_cell_name
+    input_data = input_data.makeCopy()
+    input_data.update_cell(cell_number)
+    cell_name = input_data.current_cell_name
 
-    if plotAll:
-        odor_indexes = np.arange(inputData.num_unique_odors)
+    if plot_all_cells:
+        odor_indexes = np.arange(input_data.num_unique_odors)
     else:
-        odor_indexes = np.nonzero(inputData.significance_table[cell])[0]
+        odor_indexes = np.nonzero(input_data.significance_table[cell_number])[0]
 
     DewanIOhandler.make_cell_folder4_plot(str(cell_name), *folders)
 
     for index in odor_indexes:
 
-        odor = inputData.unique_odors[index]
+        odor = input_data.unique_odors[index]
 
-        inputData.update_odor(index)
+        input_data.update_odor(index)
 
-        colormap = generate_color_map(len(inputData.current_odor_trials))
+        colormap = generate_color_map(len(input_data.current_odor_trials))
         plt.rcParams['axes.prop_cycle'] = colormap
 
         odor_name = odor
 
-        auroc_percentile = inputData.percentiles[cell][index]
+        auroc_percentile = input_data.percentiles[cell_number][index]
 
-        lower_bound = inputData.lower_bounds[cell][index]
-        upper_bound = inputData.upper_bounds[cell][index]
+        lower_bound = input_data.lower_bounds[cell_number][index]
+        upper_bound = input_data.upper_bounds[cell_number][index]
 
-        # lower_bound = 0.01  # Set upper and lower boundaries for the percentile of the AUROC value in the shuffled
-        # # AUROC distribution.
-        # upper_bound = 0.99
-
-        fig, (ax1, ax2) = plt.subplots(1, 2, width_ratios=[3, 1])
+        _, (ax1, ax2) = plt.subplots(1, 2, width_ratios=[3, 1])
         plt.suptitle(f'Cell:{cell_name} Odor:{odor_name}', fontsize=14)
         ax1.set_title('Cell Traces', fontsize=10)
         ax1.set(xlabel="Time ((s) since FV On)", ylabel="Signal")
 
         y_min = []
         y_max = []
-        data4average = []
+        data_4_average = []
         x_vals = []
 
-        for trial in inputData.current_odor_trials:
-            data2plot = inputData.Data[cell, trial, :]
-            data4average.append(data2plot)
-            x_vals = inputData.FV_time_map[trial, :]
-            min_val = np.min(data2plot)
-            y_min.append(min_val)
-            max_val = np.max(data2plot)
-            y_max.append(max_val)
-            ax1.plot(x_vals, data2plot, linewidth=0.5)
+        for trial in input_data.current_odor_trials:
+            data_2_plot = input_data.Data[cell_number, trial, :]
+            data_4_average.append(data_2_plot)
 
-        data4average = np.mean(data4average, axis=0)
-        ax1.plot(x_vals, data4average, "k", linewidth=1.5)
+            x_vals = input_data.FV_time_map[trial, :]
+
+            min_y_val = np.min(data_2_plot)
+            min_y_val = np.max(data_2_plot)
+            y_min.append(min_y_val)
+            y_max.append(min_y_val)
+
+            ax1.plot(x_vals, data_2_plot, linewidth=0.5)
+
+        data_4_average = np.mean(data_4_average, axis=0)
+        ax1.plot(x_vals, data_4_average, "k", linewidth=1.5)
 
         ax1.text(0.015, 0.02, f'AUROC Percentile: {str(auroc_percentile*100)}',
                  transform=ax1.transAxes, fontsize='x-small', style='italic',
@@ -99,8 +103,8 @@ def plot_cell_odor_traces(inputData: DewanDataStore.PlottingDataStore, latentCel
 
         y_min = min(y_min)
         y_max = max(y_max)
-        x_min = min(inputData.FV_time_map[0, :])
-        x_max = max(inputData.FV_time_map[0, :])
+        x_min = min(input_data.FV_time_map[0, :])
+        x_max = max(input_data.FV_time_map[0, :])
 
         ax1.set_xlim([x_min, x_max])
         ax1.set_ylim([y_min - (0.01 * y_min), y_max + (0.01 * y_max)])
@@ -113,15 +117,15 @@ def plot_cell_odor_traces(inputData: DewanDataStore.PlottingDataStore, latentCel
         # fv_rectangle = mpatches.Rectangle((0, rectangle_y_min), 2, rectangle_y_max,
         #                                  alpha=0.3, facecolor='red')
 
-        baselineXStart = inputData.FV_time_map[0, min(inputData.baseline_start_indexes[cell][index])]
-        baselineXEnd = inputData.FV_time_map[0, max(inputData.baseline_end_indexes[cell][index])]
-        evokedXStart = inputData.FV_time_map[0, min(inputData.evoked_start_indexes[cell][index])]
-        evokedXEnd = inputData.FV_time_map[0, max(inputData.evoked_end_indexes[cell][index])]
+        baseline_x_start = input_data.FV_time_map[0, min(input_data.baseline_start_indexes[cell_number][index])]
+        baseline_x_end = input_data.FV_time_map[0, max(input_data.baseline_end_indexes[cell_number][index])]
+        evoked_x_start = input_data.FV_time_map[0, min(input_data.evoked_start_indexes[cell_number][index])]
+        evoked_y_start = input_data.FV_time_map[0, max(input_data.evoked_end_indexes[cell_number][index])]
 
-        baseline_rectangle = mpatches.Rectangle((baselineXStart, rectangle_y_min), (baselineXEnd - baselineXStart),
+        baseline_rectangle = mpatches.Rectangle((baseline_x_start, rectangle_y_min), (baseline_x_end - baseline_x_start),
                                                 rectangle_y_max, alpha=0.3, facecolor='blue')
 
-        evoked_rectangle = mpatches.Rectangle((evokedXStart, rectangle_y_min), (evokedXEnd - evokedXStart),
+        evoked_rectangle = mpatches.Rectangle((evoked_x_start, rectangle_y_min), (evoked_y_start - evoked_x_start),
                                               rectangle_y_max,
                                               alpha=0.3, facecolor='green')
 
@@ -129,9 +133,9 @@ def plot_cell_odor_traces(inputData: DewanDataStore.PlottingDataStore, latentCel
         ax1.add_patch(baseline_rectangle)
         ax1.add_patch(evoked_rectangle)
 
-        plot_evoked_baseline_means(inputData, ax2, colormap)
+        plot_evoked_baseline_means(input_data, ax2, colormap)
 
-        filename = f'{inputData.file_header}Cell{cell_name}-{odor_name}-CellTrace.pdf'
+        filename = f'{input_data.file_header}Cell{cell_name}-{odor_name}-CellTrace.pdf'
 
         path = Path(
             '.', 'ImagingAnalysis', 'Figures', 'AllCellTracePlots', folder, f'Cell-{cell_name}', filename)
@@ -142,8 +146,9 @@ def plot_cell_odor_traces(inputData: DewanDataStore.PlottingDataStore, latentCel
         plt.close()
 
 
-def plot_evoked_baseline_means(inputData: DewanDataStore.PlottingDataStore, ax2: plt.axis, colormap: cycler):
-    baseline_means, evoked_means = DewanTraceTools.averageTrialData(*DewanTraceTools.collect_trial_data(inputData))
+def plot_evoked_baseline_means(input_data: DewanDataStore.PlottingDataStore, ax2: plt.axis, colormap: cycler):
+    baseline_data, evoked_data = DewanTraceTools.collect_trial_data(input_data) # Get the baseline and evoked df/F data
+    baseline_means, evoked_means = DewanTraceTools.averageTrialData(baseline_data, evoked_data) # Get means of the baseline/evoked data
 
     x_val = [[1], [2]]
     x_vals = np.tile(x_val, (1, len(baseline_means)))
@@ -166,15 +171,15 @@ def plot_evoked_baseline_means(inputData: DewanDataStore.PlottingDataStore, ax2:
     ax2.plot(x_val, (baseline_mean, evoked_mean), '--ok', linewidth=3)
 
 
-def plot_cells(inputData: DewanDataStore.PlottingDataStore, latentCellsOnly: bool = False, plotAll: bool = False) -> None:
-    thread_pool = Pool()
+def pooled_cell_plotting(input_data: DewanDataStore.PlottingDataStore, latent_cells_only: bool = False, plot_all_cells: bool = False) -> None:
+    thread_pool = multiprocessing.Pool()
 
-    partial_function = partial(plot_cell_odor_traces, inputData, latentCellsOnly, plotAll)
+    partial_function = partial(plot_cell_odor_traces, input_data, latent_cells_only, plot_all_cells)
 
-    if plotAll:
-        cells = range(inputData.number_cells)
+    if plot_all_cells:
+        cells = range(input_data.number_cells)
     else:
-        cells = np.unique(np.nonzero(inputData.significance_table > 0)[0])
+        cells = np.unique(np.nonzero(input_data.significance_table > 0)[0])
         # Get only the cells that had some type of significant response
 
     thread_pool.map(partial_function, cells)
@@ -182,8 +187,8 @@ def plot_cells(inputData: DewanDataStore.PlottingDataStore, latentCellsOnly: boo
     thread_pool.join()
 
 
-def plot_significance_matricies(inputData: DewanDataStore.PlottingDataStore, latentCellsOnly: bool = False) -> None:
-    if latentCellsOnly:
+def plot_significance_matricies(input_data: DewanDataStore.PlottingDataStore, latent_cells_only: bool = False) -> None:
+    if latent_cells_only:
         folder = 'LatentCells'
         title = 'Latent'
     else:
@@ -191,15 +196,15 @@ def plot_significance_matricies(inputData: DewanDataStore.PlottingDataStore, lat
         title = 'OnTime'
 
     fig, ax = plt.subplots()
-    unique_odor_labels = inputData.unique_odors
+    unique_odor_labels = input_data.unique_odors
     ax.set_facecolor((1, 1, 1))
     colormap = ListedColormap(['yellow', 'red', 'green'])
-    ax.imshow(inputData.significance_table, cmap=colormap,
-              extent=(0, inputData.num_unique_odors, inputData.number_cells, 0))
-    ax.set_yticks(np.arange(inputData.number_cells), labels=[])
-    ax.set_xticks(np.arange(inputData.num_unique_odors), labels=[])
-    ax.set_xticks(np.arange(0.5, inputData.num_unique_odors + 0.5, 1), labels=unique_odor_labels, minor=True)
-    ax.set_yticks(np.arange(0.5, inputData.number_cells + 0.5, 1), labels=inputData.Cell_List, minor=True)
+    ax.imshow(input_data.significance_table, cmap=colormap,
+              extent=(0, input_data.num_unique_odors, input_data.number_cells, 0))
+    ax.set_yticks(np.arange(input_data.number_cells), labels=[])
+    ax.set_xticks(np.arange(input_data.num_unique_odors), labels=[])
+    ax.set_xticks(np.arange(0.5, input_data.num_unique_odors + 0.5, 1), labels=unique_odor_labels, minor=True)
+    ax.set_yticks(np.arange(0.5, input_data.number_cells + 0.5, 1), labels=input_data.Cell_List, minor=True)
 
     ax.tick_params(axis='both', which='both', left=False, bottom=False)
 
@@ -209,8 +214,8 @@ def plot_significance_matricies(inputData: DewanDataStore.PlottingDataStore, lat
     plt.xlabel("Odors")
     plt.ylabel("Cells")
 
-    ax.set_xlim([0, inputData.num_unique_odors])
-    ax.set_ylim([0, inputData.number_cells])
+    ax.set_xlim([0, input_data.num_unique_odors])
+    ax.set_ylim([0, input_data.number_cells])
     red_patch = mpatches.Patch(color='red', label='Negative AUROC')
     green_patch = mpatches.Patch(color='green', label='Positive AUROC')
     yellow_patch = mpatches.Patch(color='yellow', label='No Significance')
@@ -222,36 +227,36 @@ def plot_significance_matricies(inputData: DewanDataStore.PlottingDataStore, lat
     fig.tight_layout(pad=0.2)
 
     folders = Path(*['.', 'ImagingAnalysis', 'Figures', 'AUROCPlots', folder])
-    filename = f'{inputData.file_header}{title}AllCellsvOdorsAUROC.svg'
+    filename = f'{input_data.file_header}{title}AllCellsvOdorsAUROC.svg'
     plt.savefig(folders.joinpath(filename), dpi=1200, bbox_inches="tight")
     plt.show()
     plt.close()
 
 
-def plot_auroc_distributions(fileHeader, auroc_shuffle, auroc, ub, lb, CellList, cellNum, uniqueOdors, odori,
-                             latentCellsOnly: bool = False) -> None:
-    cell_name = str(CellList[cellNum])
-    odor_name = str(uniqueOdors[odori])
+def plot_auroc_distributions(file_header, auroc_shuffle, auroc, ub, lb, cell_list, cell_number, unique_odors, odor_index,
+                             latent_cells_only: bool = False) -> None:
+    cell_name = str(cell_list[cell_number])
+    odor_name = str(unique_odors[odor_index])
 
     fig, ax = plt.subplots()
     plt.hist(auroc_shuffle, bins=10)
     plt.axvline(x=ub, color='b'), plt.axvline(x=lb, color='b'), plt.axvline(x=auroc, color='r')
     plt.title(f'Cell:{cell_name} {odor_name}')
 
-    if latentCellsOnly:
+    if latent_cells_only:
         sub_folder = 'LatentCells'
     else:
         sub_folder = 'OnTimeCells'
 
     folder = Path(*['.', 'ImagingAnalysis', 'Figures', 'AUROCPlots', f'{sub_folder}'])
-    filename = f'{fileHeader}Cell{cell_name}-{odor_name}.pdf'
+    filename = f'{file_header}Cell{cell_name}-{odor_name}.pdf'
     plt.savefig(folder.joinpath(filename))
     plt.close()
 
 
-def plot_trial_variances(inputData: DewanDataStore.AUROCdataStore, SignificanceTable: np.array,
+def plot_trial_variances(input_data: DewanDataStore.AUROCdataStore, significance_table: np.array,
                          latentCells: bool = False) -> None:
-    responsive_cells_truth_table = np.any(SignificanceTable, axis=1)  # Find all rows that are not all zeros
+    responsive_cells_truth_table = np.any(significance_table, axis=1)  # Find all rows that are not all zeros
     responsive_cell_list_index = np.nonzero(responsive_cells_truth_table)[0]  # Only keep cells that are not
 
     if latentCells:
@@ -262,15 +267,15 @@ def plot_trial_variances(inputData: DewanDataStore.AUROCdataStore, SignificanceT
     folders = ['TrialVariancePlots', folder]
 
     for cell in responsive_cell_list_index:
-        responsive_odor_indexes = np.nonzero(SignificanceTable[cell] > 0)[0]
+        responsive_odor_indexes = np.nonzero(significance_table[cell] > 0)[0]
 
-        inputData.update_cell(cell)
-        DewanIOhandler.make_cell_folder4_plot(inputData.current_cell_name, *folders)
+        input_data.update_cell(cell)
+        DewanIOhandler.make_cell_folder4_plot(input_data.current_cell_name, *folders)
 
         for odor in responsive_odor_indexes:
-            inputData.update_odor(odor)
+            input_data.update_odor(odor)
 
-            baseline_data, evok_data = DewanTraceTools.collect_trial_data(inputData, None, latentCells)
+            baseline_data, evok_data = DewanTraceTools.collect_trial_data(input_data, None, latentCells)
 
             baseline_mean = np.mean(np.hstack(baseline_data))
 
@@ -278,33 +283,24 @@ def plot_trial_variances(inputData: DewanDataStore.AUROCdataStore, SignificanceT
 
             baseline_corrected_evok_data = truncated_evok_data - baseline_mean
 
-            vertical_scatter_plot(baseline_corrected_evok_data, inputData, *folders)
+            vertical_scatter_plot(baseline_corrected_evok_data, input_data, *folders)
 
 
-def truncate_data(data):
-    row_len = np.min([len(row) for row in data])
-    # print(row_len)
-    for i, row in enumerate(data):
-        data[i] = row[:row_len]
-
-    return data
-
-
-def vertical_scatter_plot(data2Plot: list, dataInput: DewanDataStore.AUROCdataStore, *folders):
+def vertical_scatter_plot(data_2_plot: list, data_input: DewanDataStore.AUROCdataStore, *folders):
     # x = len(data2Plot)
     width = 0.6
     dotSize = 10
     fig, ax = plt.subplots()
-    for i, y in enumerate(data2Plot):
+    for i, y in enumerate(data_2_plot):
         x = np.ones(len(y)) * (i + 1) + (np.random.rand(len(y)) * width - width / 2.)
         ax.scatter(x, y, s=dotSize)
-    plt.title(f'Trials for Cell: {str(dataInput.current_cell_name)} v. {dataInput.odor_name}')
+    plt.title(f'Trials for Cell: {str(data_input.current_cell_name)} v. {data_input.odor_name}')
     plt.ylabel("Signal (Evoked - Baseline")
     plt.xlabel("Trial")
-    ax.set_xticks(range(1, len(data2Plot) + 1))
+    ax.set_xticks(range(1, len(data_2_plot) + 1))
 
-    path = Path('ImagingAnalysis', 'Figures', *folders, f'Cell-{dataInput.current_cell_name}',
-                f'{dataInput.odor_name}-TrialTraces.pdf')
+    path = Path('ImagingAnalysis', 'Figures', *folders, f'Cell-{data_input.current_cell_name}',
+                f'{data_input.odor_name}-TrialTraces.pdf')
     plt.savefig(path, dpi=800)
     plt.close()
 
@@ -345,12 +341,12 @@ def plot_distance_v_correlation(unique_distance_v_correlation):
     x = unique_distance_v_correlation[:, 0]
     y = unique_distance_v_correlation[:, 1]
 
-    # 8C.1: Quick linear regression
+    # Quick linear regression
     m, b = np.polyfit(x, y, deg=1)
     reg_x = np.arange(np.max(x))
     reg_y = np.add(np.multiply(m, reg_x), b)
 
-    # 8C.2: Plot regression line, formula, and data
+    # Plot regression line, formula, and data
     fig, ax = plt.subplots()
     ax.plot(reg_x, reg_y)
     ax.scatter(x, y)
