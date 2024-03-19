31cf80d4249d2f1ff8f025cccf10213cf82377dc
Austin Pauley <austin2118ace@gmail.com>
2024-03-19 14:24:20 -0400

    updates to devonv code so they will work with the actual trace data

--- dewan_calcium/DewanDeconv.py
+++ dewan_calcium/DewanDeconv.py
@@ -2,36 +2,37 @@ import numpy as np
 import pandas as pd
 from scipy import signal
 from sklearn import preprocessing
-from oasis.functions import deconvolve  # install using conda install to avoid having to build
+from oasis.functions import deconvolve # install using conda install to avoid having to build
 from pathlib import Path
 
 
-def deconvolve_traces(trace_file: Path, framerate: int, peak_args: dict) -> tuple[pd.DataFrame, np.ndarray]:
-    data = load_transients(trace_file)
-    scaled_data = z_score_data(data)
+def deconvolve_traces(trace_data: list, framerate: int, peak_args: dict) -> tuple[pd.DataFrame, np.ndarray]:
+    #data = load_transients(trace_file)
+    scaled_data = z_score_data(trace_data)
 
     denoised_data = []
     peaks = []
-    for each in data.columns:
-        new_trace = scaled_data[each].values
-        new_trace = smooth_data(new_trace)
-        denoised_data.append(new_trace)
-        found_peaks = find_peaks(new_trace, framerate, peak_args)
-        peaks.append(found_peaks)
-
-    denoised_data = pd.DataFrame(denoised_data)
-    peaks = np.ndarray(peaks)
+    for cell in scaled_data:
+        new_cell_traces = []
+        cell_peaks = []
+        for trial in cell:
+            new_trace = smooth_data(trial)
+            new_cell_traces.append(new_trace)
+            found_peaks = find_peaks(new_trace, framerate, peak_args)
+            cell_peaks.append(found_peaks)
+        denoised_data.append(new_cell_traces)
+        peaks.append(cell_peaks)
 
     return denoised_data, peaks
 
 
 def find_peaks(data: np.ndarray, framerate: int, peak_args: dict) -> np.ndarray:
-    width_scalar = peak_args['width']
-    distance_scalar = peak_args['distance']
+    width_time = peak_args['width']
+    distance_time = peak_args['distance']
     peak_height = peak_args['height']
 
-    peak_width = framerate * width_scalar
-    peak_distance = framerate * distance_scalar
+    peak_width = (framerate * width_time) / 1000
+    peak_distance = (framerate * distance_time) / 1000
 
     peaks = signal.find_peaks(data, height=peak_height, width=peak_width, distance=peak_distance)
 
@@ -43,47 +44,51 @@ def load_transients(path: Path) -> pd.DataFrame:
         data = pd.read_csv(path, header=0, index_col=0, dtype=object)
         data = data[1:]  # Remove first row
         
-        cols = [column[1:] for column in data.columns]  # Remove leading space from each column name
+        cols = [column[1:] for column in data.columns] # Remove leading space from each column name
         data.columns = cols
 
-        data = data.astype(np.float32)  # Cast all numbers from object -> np.float32
+        data = data.astype(np.float32) # Cast all numbers from object -> np.float32
 
-        data.interpolate(inplace=True)  # Fill all NaN with a linearly interpolated value
+        data.interpolate(inplace=True) # Fill all NaN with a linearly interpolated value
 
     except FileNotFoundError:
-        raise FileNotFoundError(f'Error, cannot find the data file: {path}')
+        raise f'Error, cannot find the data file: {path}'
     return data
 
 
 def z_score_data(data: pd.DataFrame) -> pd.DataFrame:
+    # Function is given a Cells x Trials array
+    # Zscores each trial and then returns the array
+
     from scipy.stats import zscore
 
-    keys = data.keys() # Get each column name
-    z_scored_data = dict.fromkeys(keys)
+    z_scored_data = []
 
-    for each in keys: # Loop through columns
-        col = data[each]
-        z_scored = zscore(col) # Zscore column and replace it
-        
-        z_scored_data[each] = z_scored
-        
-    z_scored_data = pd.DataFrame(z_scored_data, index=data.index)
+    for cell in data:
+        z_scored_cell = []
+        for trial in cell:
+            z_scored_trial = zscore(trial)
+            z_scored_cell.append(z_scored_trial)
+
+        z_scored_data.append(z_scored_cell)
 
     return z_scored_data
 
 
 def smooth_data(trace: tuple) -> np.ndarray:
     import warnings
-    trace_name, trace_data = trace  # Unpack tuple
-    trace_data = trace_data.values
-    print(f'Smoothing trace: {trace_name}')
+    #trace_name, trace_data = trace # Unpack tuple
+    #trace_data = trace_data.values
+    #print(f'Smoothing trace: {trace_name}')
 
+    
     warnings.simplefilter("ignore", category=UserWarning)
-    deconv_data = deconvolve(trace_data, g=(None, None), optimize_g=5, penalty=1, max_iter=5)
+    deconv_data = deconvolve(trace, g=(None, None),penalty=1)
 
     smoothed_trace = deconv_data[0]
 
-    print(f'Smoothing complete for: {trace_name}!')
+    #print(f'Smoothing complete for: {trace_name}!')
+
 
     return smoothed_trace
 
