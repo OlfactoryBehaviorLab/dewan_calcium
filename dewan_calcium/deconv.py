8c6bf757df07dfd58c3565fcf70a4d4073e5cd9e
Austin Pauley <austin2118ace@gmail.com>
2024-04-28 22:43:12 -0400

    Remove old deconvolve function Loop through smoothed data to get peaks cleanup comments

--- dewan_calcium/deconv.py
+++ dewan_calcium/deconv.py
@@ -2,49 +2,24 @@ import numpy as np
 import pandas as pd
 from scipy import signal
 from oasis.functions import deconvolve  # install using conda install to avoid having to build
-from pathlib import Path
 
 
-def deconvolve_traces(trace_data: pd.DataFrame, framerate: int, peak_args: dict):
-    denoised_data = []
-    peaks = []
-
-    scaled_data = z_score_data(trace_data)
-
-    try:
-        denoised_data = multithread_smoothing(scaled_data, num_workers=8)
-    except Exception as e:
-        denoised_data.append(np.NaN)
-
-    for cell in scaled_data:
-
-        cell_data = np.hstack(cell)
-
-        try:
-            new_trace = smooth_data(cell_data)
-            cell_peaks = find_peaks(new_trace, framerate, peak_args)
-        except UnboundLocalError:
-            new_trace = np.NAN
-            cell_peaks = np.NAN
-
-        denoised_data.append(new_trace)
-        peaks.append(cell_peaks)
-
-    return denoised_data, peaks
-
-
-def find_peaks(data: np.ndarray, framerate: int, peak_args: dict) -> np.ndarray:
-    width_time = peak_args['width']
+def find_peaks(smoothed_data: np.ndarray, framerate: int, peak_args: dict) -> list:
+    width_time = peak_args['decay']
     distance_time = peak_args['distance']
     peak_height = peak_args['height']
 
     peak_width = (framerate * width_time) / 1000
     peak_distance = (framerate * distance_time) / 1000
 
-    peaks = signal.find_peaks(data, height=peak_height, width=peak_width, distance=peak_distance)
-    peaks = peaks[0]  # Return only the indexes (x locations) of the peaks
-    return peaks
+    transient_indexes = []
 
+    for trace in smoothed_data:
+        peaks = signal.find_peaks(trace, height=peak_height, width=peak_width, distance=peak_distance)
+        peaks = peaks[0]  # Return only the indexes (x locations) of the peaks
+        transient_indexes.append(peaks)
+
+    return transient_indexes
 
 
 def z_score_data(data: pd.DataFrame) -> list:
@@ -61,8 +36,6 @@ def z_score_data(data: pd.DataFrame) -> list:
 
         combined_data = np.hstack(fluorescence_values)
         z_score_combined = zscore(combined_data)
-
-        # z_scored_cell = np.split(z_score_combined, num_trials)
 
         z_scored_data.append(z_score_combined)
 
@@ -83,7 +56,6 @@ def calc_smoothing_params(framerate, decay_time, rise_time):
 def smooth_data(trace, calc_kernel) -> np.ndarray:
     import warnings
 
-    #g1, g2 = calc_smoothing_params(10, .4, 0.08)
     g1, g2 = calc_kernel
 
     warnings.simplefilter("ignore", category=UserWarning)
