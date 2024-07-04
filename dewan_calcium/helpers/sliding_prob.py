519c97b3cd559c4b2a2487bdce1b2cfa9f8013fe
austin2118ace <austin2118ace@gmail.com>
2024-07-04 01:23:58 -0400

    Add small prep function, make iterators a constant

--- dewan_calcium/helpers/sliding_prob.py
+++ dewan_calcium/helpers/sliding_prob.py
@@ -8,13 +8,24 @@ values provided as arguments
 """
 import numpy as np
 
+SLID_PROB_INC = 100
 
-def sliding_probability(data, start_range: int, end_range: int, increments: int) -> np.array:
+
+def sliding_probability(data, start_range: int, end_range: int) -> np.array:
     prob_vector = []
-    bin_steps = (end_range - start_range) / increments
+    bin_steps = (end_range - start_range) / SLID_PROB_INC
     bins = np.arange(start_range, end_range, bin_steps)
 
     for current_bin in bins:
         prob_vector.append(sum(np.ravel(data > current_bin)) / len(np.ravel(data)))
 
     return prob_vector
+
+
+def prep_probabilities(data: np.array):
+
+    data.reverse()
+    data.insert(0, 0.0)
+    data.insert(len(data) + 1, 1.0)
+
+    return data
