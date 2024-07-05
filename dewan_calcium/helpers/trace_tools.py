19ab1b5fd91293bb5e48f21b979a6b2b60392cf9
austin2118ace <austin2118ace@gmail.com>
2024-07-05 18:40:01 -0400

    new trial data collection function

--- dewan_calcium/helpers/trace_tools.py
+++ dewan_calcium/helpers/trace_tools.py
@@ -7,12 +7,42 @@ import pandas as pd
 from .data_stores import AUROCdataStore, AUROCReturn # Import for typedef
 
 
-def new_collect_trial_data(odor_df:  pd.DataFrame, time_df: pd.DataFrame, latent: bool = False):
+def new_collect_trial_data(odor_df:  pd.DataFrame, time_df: pd.DataFrame, response_duration: int, latent: bool = False):
+    """
+
+    Args:
+        odor_df: Pandas DataFrame containing all the trials for a specific odor
+        time_df: Pandas DataFrame containing all the timestamps for the trials for a sepcific odor
+        response_duration: int reflecting the amount of time that the "response" period envelops
+        latent: True if we're looking for latent responses, response start time is offset by response_duration
+
+    Returns:
+
+    """
+
     baseline_data = []
     evoked_data = []
 
+    baseline_indices = []
+    evoked_indices = []
 
+    for trial_index, (_, data) in enumerate(odor_df.items()):
+        evoke_end_time = response_duration
+        if latent:
+            evoke_end_time *= 2
 
+        trial_timestamps = time_df.iloc[:, trial_index]
+
+        baseline_trial_indices = trial_timestamps[trial_timestamps < 0].index
+        baseline_trial_data = data[baseline_trial_indices]
+
+        baseline_data.append(baseline_trial_data)
+        baseline_indices.append((baseline_trial_indices[0], baseline_trial_indices[-1]))
+
+        evoked_trial_indices = trial_timestamps[trial_timestamps.between(0, evoke_end_time, 'both')].index
+        evoked_trial_data = data[evoked_trial_indices]
+        evoked_data.append(evoked_trial_data)
+        evoked_indices.append((evoked_trial_indices[0], evoked_trial_indices[-1]))
 
 
 
