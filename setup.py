85ac73bd4105287f9aaf6fc716156b4651752bbf
austin2118ace <austin2118ace@gmail.com>
2024-07-11 17:25:31 -0400

    Update package URL, make sure numpy<2.0

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 
 setup(
-	name = 'dewan_calcium',
-	description = 'Dewan Calcium Imaging Toolbox',
-	version = '1.0',
-	author = 'Dewan Lab, Florida State University',
-	author_email = 'olfactorybehaviorlab@gmail.com',
-	url = 'https://github.com/OlfactoryBehaviorLab/dewan-calcium',
+	name='dewan_calcium',
+	description='Dewan Calcium Imaging Toolbox',
+	version='1.0',
+	author='Dewan Lab, Florida State University',
+	author_email='olfactorybehaviorlab@gmail.com',
+	url='https://github.com/OlfactoryBehaviorLab/dewan_calcium',
 	packages=['dewan_calcium'],
 	python_requires=">=3.7, <3.11",
-	install_requires =	[	
-						'numpy',
+	install_requires=[
+						'numpy<2.0.0',
 						'pandas',
 						'matplotlib',
 						'scikit-learn',
