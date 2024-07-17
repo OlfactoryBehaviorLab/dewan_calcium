fc66c4529a2062a51348d24ad235546dc6384d8a
austin2118ace <austin2118ace@gmail.com>
2024-07-16 22:02:57 -0400

    Move plotting functions from old file to new file

--- dewan_calcium/plotting.py
+++ dewan_calcium/plotting.py
@@ -337,3 +337,32 @@ def plot_distance_v_correlation(unique_distance_v_correlation):
     plt.ylabel("Pairwise Signal Correlation")
     plt.title("Activity vs. Spatial Distance")
     _ = ax.text(0, np.max(y), f'y={np.round(m, 4)}x + {np.round(b, 4)}')
+
+
+
+def plot_epm_roi(polygons: dict, image):
+    fig, ax = plt.subplots()  # New Graph
+    ax.set_axis_off()
+
+    open_coordinates, closed_coordinates, center_coordinates = get_polygon_coordinates(polygons)
+
+    open_poly = Polygon(open_coordinates, alpha=0.2, color='r')
+    closed_poly = Polygon(closed_coordinates, alpha=0.2, color='b')
+    center_poly = Polygon(center_coordinates, alpha=0.3, color=(0, 1, 0))
+
+    patches = PatchCollection([open_poly, closed_poly, center_poly], match_original=True)
+
+    ax.add_collection(patches)
+    ax.legend([open_poly, closed_poly, center_poly], ['Open', 'Closed', 'Center'], loc='lower center', framealpha=1)
+    _ = ax.imshow(image)
+
+    return fig, ax
+
+
+def get_polygon_coordinates(polygons: dict):
+    coordinates = []
+
+    for polygon in polygons['Polygon']:
+        coordinates.append(list(polygon.exterior.coords)[:-1])  # Drop last point as it is a duplicate of the first
+
+    return coordinates
