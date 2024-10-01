e7e7f23755b68c3cd67600f1f6f5f7b23a98b2d4
austin2118ace <austin2118ace@gmail.com>
2024-09-30 23:54:29 -0400

    Change pseudotrial function to just get pairs of indices

--- dewan_calcium/helpers/HFvFM.py
+++ dewan_calcium/helpers/HFvFM.py
@@ -2,13 +2,14 @@ import numpy as np
 
 def get_pseudotrials(cell_trace_indices, PSEUDOTRIAL_LEN_S, ENDOSCOPE_FRAMERATE):
     pseudotrial_indices = []
+def get_pseudotrial_pairs(trial_length, PSEUDOTRIAL_LEN_S, ENDOSCOPE_FRAMERATE):
 
     pseudotrial_length_frames = PSEUDOTRIAL_LEN_S * ENDOSCOPE_FRAMERATE
 
-    for name, (start, end) in cell_trace_indices.iterrows():
-        frame_numbers = np.arange(start, end+1, pseudotrial_length_frames)
-        frame_pairs = list(zip(frame_numbers[:-1], frame_numbers[1:]+1))
-        pseudotrial_indices.append(frame_pairs)
+    frame_numbers = np.arange(0, trial_length+1, pseudotrial_length_frames)
+    frame_pairs = list(zip(frame_numbers[:-1], frame_numbers[1:]))
+
+    return frame_pairs
 
     return pseudotrial_indices
 
