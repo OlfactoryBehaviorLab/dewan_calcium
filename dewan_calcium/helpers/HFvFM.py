55e95adf4b567c81186da76292b5407175a511f4
austin2118ace <austin2118ace@gmail.com>
2024-09-30 22:30:12 -0400

    Return an alternating pattern of trial names accounting for odd numbers of trials, and varying which trial type comes first

--- dewan_calcium/helpers/HFvFM.py
+++ dewan_calcium/helpers/HFvFM.py
@@ -11,3 +11,21 @@ def get_pseudotrials(cell_trace_indices, PSEUDOTRIAL_LEN_S, ENDOSCOPE_FRAMERATE)
         pseudotrial_indices.append(frame_pairs)
 
     return pseudotrial_indices
+
+def get_trial_labels(num_trials, HF_first):
+    import math
+    trial_labels = []
+    temp_num_trials = math.ceil(num_trials / 2)
+
+    for i in range(temp_num_trials):
+        if HF_first:
+            trial_labels.append(f'HF-{i + 1}')
+            trial_labels.append(f'FM-{i + 1}')
+        else:
+            trial_labels.append(f'FM-{i + 1}')
+            trial_labels.append(f'HF-{i + 1}')
+
+    if num_trials % 2 == 1:
+        trial_labels = trial_labels[:-1]
+
+    return trial_labels
