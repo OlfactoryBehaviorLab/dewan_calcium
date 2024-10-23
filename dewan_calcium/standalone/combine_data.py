6c6d96c7bf1239fb47e269200760682a642e1060
austin2118ace <austin2118ace@gmail.com>
2024-10-23 10:30:36 -0400

    Search for the combined data files

--- dewan_calcium/standalone/combine_data.py
+++ dewan_calcium/standalone/combine_data.py
@@ -2,11 +2,22 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
-folder = Path('R:\2_Inscopix\1_DTT\1_OdorAnalysis\1_Concentration')
+folder = Path('R:\\2_Inscopix\\1_DTT\\1_OdorAnalysis\\2_Identity')
+
+
+def find_data_files():
+    print('Searching for data files, this may take a while...')
+    data_files = folder.glob('*\\Analysis\\Output\\combined\\*combined_data_shift.pickle')
+    if not data_files:
+        print('No data files found.')
+        return None
+    else:
+        data_files = list(data_files)
+        print(f'Found {len(data_files)} data files.')
+        return list(data_files)
 
 def main():
-    data_files = folder.rglob('*combined_data_shift.pickle')
-    print(data_files)
+    data_files = find_data_files()
 
 
 
