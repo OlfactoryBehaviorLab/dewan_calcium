cc76beca53658d5f4ba778ae9d4076f55487d766
austin2118ace <austin2118ace@gmail.com>
2024-10-24 16:18:38 -0400

    reorganize function order

--- dewan_calcium/standalone/combine_data.py
+++ dewan_calcium/standalone/combine_data.py
@@ -67,6 +67,29 @@ def find_data_files(exp_type: str):
         return list(data_files)
 
 
+def update_cell_names(combined_data):
+    """
+    Function which will take each cell number, and add it to a string prepended with a 'C.' The dataframe can then be
+    indexed by unique cell names instead of an integer. This reflects the nomenclature in the normal processing
+    notebooks. This is necessitated by how strings are sorted when dataframes are concatenated.
+    Args:
+        combined_data (pd.DataFrame): Reference to a Dataframe containing the combined data from n number of
+        experiments. This dataframe is indexed by a MultiIndex that is size cells x (odorants * # of trials)
+        Cells are represented by integers instead of labels of type string.
+
+    Returns:
+        Input is passed as a reference and the original object is modified in-place
+
+    """
+
+    string_names = [f'C{i}' for i in range(len(combined_data.columns.levels[0]))]
+    combined_data.columns = combined_data.columns.set_levels(string_names, level=0)
+
+
+def generate_new_numbers(new_cells: int, total: int):
+    new_total = new_cells + total
+    return list(range(total, new_total))
+
 def main():
     output_dir = []
     combined_data = []
@@ -92,30 +115,6 @@ def main():
         output_dir.mkdir()
 
     pd.to_pickle(combined_data, output_path)
-
-
-def update_cell_names(combined_data):
-    """
-    Function which will take each cell number, and add it to a string prepended with a 'C.' The dataframe can then be
-    indexed by unique cell names instead of an integer. This reflects the nomenclature in the normal processing
-    notebooks. This is necessitated by how strings are sorted when dataframes are concatenated.
-    Args:
-        combined_data (pd.DataFrame): Reference to a Dataframe containing the combined data from n number of
-        experiments. This dataframe is indexed by a MultiIndex that is size cells x (odorants * # of trials)
-        Cells are represented by integers instead of labels of type string.
-
-    Returns:
-        Input is passed as a reference and the original object is modified in-place
-
-    """
-
-    string_names = [f'C{i}' for i in range(len(combined_data.columns.levels[0]))]
-    combined_data.columns = combined_data.columns.set_levels(string_names, level=0)
-
-
-def generate_new_numbers(new_cells: int, total: int):
-    new_total = new_cells + total
-    return list(range(total, new_total))
 
 
 if __name__ == "__main__":
