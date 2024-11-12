cd0ce80ebc7eaf4e080d8198e74ed5cf6cf9884c
austin2118ace <austin2118ace@gmail.com>
2024-11-12 14:32:46 -0500

    wrap this in a try:catch incase the user doesn't set ISX, it will default to not attempting to load the module

--- dewan_calcium/__init__.py
+++ dewan_calcium/__init__.py
@@ -1,8 +1,12 @@
 import os
+import warnings
+try:
+    if os.environ['ISX'] == '1':
+        try:
+            import isx
+        except ImportError as error:
+            print('The Inscopix Data Processing Software (IDPS) API is not installed!')
+            raise error
+except KeyError as ke:
+    warnings.warn('The ISX environment variable is not present, assuming IDPS API is not needed...')
 
-if os.environ['ISX'] == '1':
-    try:
-        import isx
-    except ImportError as error:
-        print('The Inscopix Data Processing Software (IDPS) API is not installed!')
-        raise error
