26359d471d12ec400da3e44c44b0c14eee97e979
Austin Pauley <austin2118ace@gmail.com>
2025-01-09 14:25:34 -0500

    Remove uneeded functions; refactor to only include one set of files; update to iterate folders properly

--- dewan_calcium/standalone/get_project_files.py
+++ dewan_calcium/standalone/get_project_files.py
@@ -1,95 +1,77 @@
-from pathlib import Path
-
 import os
 os.environ['ISX'] = '0'
 
+from pathlib import Path
 
-def get_folders(input_dir: Path, animal_type: list):
+
+def get_folders(input_dir: Path, exp_type, animal_type: list) -> dict:
     if not input_dir.exists():
         raise FileNotFoundError(f'Input directory {input_dir} does not exist!')
 
-    folders_per_type = {_type:[] for _type in animal_type}
+    data_files = {}
 
     for _dir in input_dir.iterdir():
         for _type in animal_type:
+            data_files[_type] = {}
             if _type in str(_dir):
                 type_dir = list(_dir.iterdir())
-                folders_per_type[_type].extend(type_dir)
+                for animal in type_dir:
+                    animal_files = _find_data_files(animal, exp_type, error=True)
+                    print(animal)
+                    data_files[_type][animal.name] = animal_files
 
-    return folders_per_type
+    return data_files
 
 
-def find_data_files(animal_dir: Path, exp_type: str, error: bool = False):
-    # return_dict = {'old': False, 'time_old': False}
-    return_dict = dict.fromkeys(['file', 'sig', 'time', 'folder'], [])
-    return_dict['folder'] = animal_dir
+def _find_data_files(animal_dir: Path, exp_type: str, error: bool = False):
+
     if exp_type == 'Concentration' or exp_type == 'Identity':
+        return_dict = dict.fromkeys(['file', 'sig', 'time', 'odor', 'folder'], [])
+        return_dict['folder'] = animal_dir
+
         data_file = animal_dir.glob('Analysis/Output/combined/*combined_data_shift.pickle')
-        new_old_data = animal_dir.glob('ImagingAnalysis/CombinedData/new*CombinedData*.pickle')
-        new_FV_timestamps = animal_dir.glob('Analysis/Preprocessed/*FV_timestamps*.pickle')
-        new_old_FV_timestamps = animal_dir.glob('ImagingAnalysis/AUROCImports/new*FVTimeMap*.pickle')
-        significance_data_old = animal_dir.glob('*SignificanceTable.xlsx')
-        significance_data_new = animal_dir.glob('Analysis/Output/*SignificanceTable.xlsx')
+        FV_timestamps = animal_dir.glob('Analysis/Preprocessed/*FV_timestamps*.pickle')
+        significance_file = animal_dir.glob('Analysis/Output/*SignificanceTable.xlsx')
+        odor_data_file = animal_dir.glob('Raw_Data/*.xlsx')
     else:
         raise ValueError(f'{exp_type} not implemented!')
 
     data_file = list(data_file)
-    new_old_data = list(new_old_data)
+    FV_timestamps_file = list(FV_timestamps)
+    significance_file = list(significance_file)
+    odor_data_file = list(odor_data_file)
 
-    new_FV_timestamps_file = list(new_FV_timestamps)
-    new_old_FV_timestamps = list(new_old_FV_timestamps)
-
-    significance_data_old = list(significance_data_old)
-    significance_data_new = list(significance_data_new)
-
-    if new_old_data:
-        return_dict['file'] = new_old_data[0]
-    elif data_file:
+    if data_file:
         return_dict['file'] = data_file[0]
     else:
         if error:
-            raise FileNotFoundError(f'There are no combined data files in {animal_dir}!')
+           print(f'There are no combined data files in {animal_dir}!')
         else:
             return_dict['file'] = None
 
-    if significance_data_old:
-        return_dict['sig'] = significance_data_old[0]
-    elif significance_data_new:
-        return_dict['sig'] = significance_data_new[0]
+    if FV_timestamps_file:
+        return_dict['time'] = FV_timestamps_file[0]
     else:
         if error:
-            raise FileNotFoundError(f'Cannot find the significance chart in {animal_dir}')
-        else:
-            return_dict['sig'] = None
-
-    if new_FV_timestamps_file:
-        return_dict['time'] = new_FV_timestamps_file[0]
-    elif new_old_FV_timestamps:
-        return_dict['time'] = new_old_FV_timestamps[0]
-    else:
-        if error:
-            raise FileNotFoundError(f'Cannot find the FV timestamps in {animal_dir}')
+            print(f'Cannot find the FV timestamps in {animal_dir}')
         else:
             return_dict['time'] = None
 
+    if significance_file:
+        return_dict['sig'] = significance_file[0]
+    else:
+        if error:
+           print(f'Cannot find the significance chart in {animal_dir}')
+        else:
+            return_dict['sig'] = None
+
+    if odor_data_file:
+        return_dict['odor'] = odor_data_file[0]
+    else:
+        if error:
+            print(f'Cannot find the odor data in {animal_dir}')
+        else:
+            return_dict['odor'] = None
+
     return return_dict
-
-
-def get_test_files(animal_dirs: list[Path]):
-    file_map = {}
-    for dir in animal_dirs:
-        trace_data = dir.glob('*combined*.pickle')
-        sig_table = dir.glob('*Significance*.xlsx')
-        odor_data = dir.glob('*OdorList.xlsx')
-        FV_times = dir.glob('*FV_timestamps.pickle')
-
-        dir_dict = {
-            'trace': list(trace_data)[0],
-            'sig': list(sig_table)[0],
-            'odor': list(odor_data)[0],
-            'time': list(FV_times)[0]
-        }
-        file_map[dir.name] = dir_dict
-
-    return file_map
 
