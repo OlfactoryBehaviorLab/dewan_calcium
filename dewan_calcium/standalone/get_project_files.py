81a4dc221198193f83235770d883f6950223f845
austin2118ace <austin2118ace@gmail.com>
2025-03-05 17:01:00 -0500

    get block->odor file; cleanup code

--- dewan_calcium/standalone/get_project_files.py
+++ dewan_calcium/standalone/get_project_files.py
@@ -1,6 +1,3 @@
-import os
-os.environ['ISX'] = '0'
-
 from pathlib import Path
 
 
@@ -10,34 +7,40 @@ def get_folders(input_dir: Path, exp_type, animal_type: list, error:bool = False
 
     data_files = dict.fromkeys(animal_type, {})
 
-    for _dir in input_dir.iterdir():
-        for _type in animal_type:
+    for _dir in input_dir.iterdir(): # Loop through the root directory
+        for _type in animal_type: # Find the folder for our animal type [VGAT/VGLUT/OXTR/etc.]
             if _type in str(_dir):
                 data_files[_type] = {}
-                type_dir = list(_dir.iterdir())
+                type_dir = list(_dir.iterdir()) # List all the animals within a class
                 for animal in type_dir:
                     animal_files = _find_data_files(animal, exp_type, error=error)
                     if not animal_files:
                         continue
 
-                    data_files[_type][animal.name] = {}
                     data_files[_type][animal.name] = animal_files
 
     return data_files
 
 
-def _find_data_files(animal_dir: Path, exp_type: str, error: bool = False):
+def _check_file(path, name):
+    if path:
+        return path[0]
+    else:
+        print(f'Unable to find {name}!')
+        return None
 
-    return_none = False
+
+def _find_data_files(animal_dir: Path, exp_type: str, error: bool = False):
+    return_dict = {}
 
     if exp_type == 'Concentration' or exp_type == 'Identity':
-        return_dict = dict.fromkeys(['file', 'sig', 'time', 'odor', 'folder'], [])
         return_dict['folder'] = animal_dir
 
         data_file = animal_dir.glob('Analysis/Output/combined/*combined_data_shift.pickle')
         FV_timestamps = animal_dir.glob('Analysis/Preprocessed/*FV_timestamps*.pickle')
         significance_file = animal_dir.glob('Analysis/Output/*SignificanceTable.xlsx')
-        odor_data_file = animal_dir.glob('Raw_Data/*.xlsx')
+        odor_data_file = animal_dir.glob('Raw_Data/*List*.xlsx')
+        block_data_file = animal_dir.glob('Raw_Data/*Blocks*.xlsx')
     else:
         raise ValueError(f'{exp_type} not implemented!')
 
@@ -45,40 +48,17 @@ def _find_data_files(animal_dir: Path, exp_type: str, error: bool = False):
     FV_timestamps_file = list(FV_timestamps)
     significance_file = list(significance_file)
     odor_data_file = list(odor_data_file)
+    block_data_file = list(block_data_file)
 
-    if data_file:
-        return_dict['file'] = data_file[0]
-    else:
-        if error:
-           print(f'There are no combined data files in {animal_dir}!')
-        else:
-            return_none = True
+    return_dict['file'] = _check_file(data_file, 'combined data file')
+    return_dict['time'] = _check_file(FV_timestamps_file, 'FV timestamps file')
+    return_dict['sig'] = _check_file(significance_file, 'significance matrix file')
+    return_dict['odor'] = _check_file(odor_data_file, 'odor data file file')
+    return_dict['block'] = _check_file(block_data_file, 'odor-block mapping file')
 
-    if FV_timestamps_file:
-        return_dict['time'] = FV_timestamps_file[0]
-    else:
-        if error:
-            print(f'Cannot find the FV timestamps in {animal_dir}')
-        else:
-            return_none = True
+    results = [True if return_dict[key] is None else False for key in return_dict.keys()]
 
-    if significance_file:
-        return_dict['sig'] = significance_file[0]
-    else:
-        if error:
-           print(f'Cannot find the significance chart in {animal_dir}')
-        else:
-            return_none = True
-    if odor_data_file:
-        return_dict['odor'] = odor_data_file[0]
-    else:
-        if error:
-            print(f'Cannot find the odor data in {animal_dir}')
-        else:
-            return_none = True
-
-    if return_none:
+    if sum(results) > 0:
         return None
     else:
         return return_dict
-
