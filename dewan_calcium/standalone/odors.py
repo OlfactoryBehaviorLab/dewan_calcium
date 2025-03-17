16819286c22b1a2ab229277b3b4c3bfae0788e9c
Austin Pauley <austin2118ace@gmail.com>
2025-03-17 13:48:42 -0400

    Add module to manually normalize our odor names across experiments

--- dewan_calcium/standalone/odors.py
+++ dewan_calcium/standalone/odors.py
@@ -3,9 +3,31 @@ def normalize_odors(odor_list, experiment_type, cell_class):
     if experiment_type == 'Identity':
         if cell_class == 'VGAT':
             for odor in odor_list:
-                if "2" in odor:
-                    normalized.append(odor[1:])
-                else:
-                    normalized.append(odor)
+                normalized.append(if_two_remove(odor))
+
+    elif experiment_type == 'Concentration':
+        for odor in odor_list:
+            _odor = if_sep_remove(odor, ['-', '_'])
+            normalized.append(if_two_remove(_odor))
 
     return normalized
+
+def if_two_remove(odor):
+    if "2" in odor:
+        return odor[1:]
+    else:
+        return odor
+
+def if_sep_remove(odor, separators):
+    for separator in separators:
+        if separator in odor:
+            odor_parts = odor.split(separator)
+            try:
+                _ = int(odor_parts[0])
+                _odor = "".join([odor_parts[1], odor_parts[0]])
+            except ValueError:
+                _odor = "".join([odor_parts[0], odor_parts[1]])
+
+            return _odor
+
+    return odor
