9225e3bf2cb2ce9db7ea2af37974efe46e69f647
Austin Pauley <austin2118ace@gmail.com>
2025-04-08 22:57:17 -0400

    numba compilation/JIT decorators

--- dewan_calcium/helpers/sliding_prob.py
+++ dewan_calcium/helpers/sliding_prob.py
@@ -7,10 +7,16 @@ Computes sliding probability for a data set based on max, min and increment
 values provided as arguments
 """
 import numpy as np
+# from numba.pycc import CC
+from numba import njit
+
+# cc=CC('sliding_prob_numba')
+
 
 SLID_PROB_INC = 100
 
-
+# @cc.export('sliding_prob', '(f8[:], i4, i4)')
+# @njit
 def sliding_probability(data, start_range: int, end_range: int) -> np.array:
     prob_vector = []
     bin_steps = (end_range - start_range) / SLID_PROB_INC
@@ -21,7 +27,7 @@ def sliding_probability(data, start_range: int, end_range: int) -> np.array:
 
     return prob_vector
 
-
+# @cc.export('prep_prob', '(f8[:],)')
 def prep_probabilities(data: np.array):
 
     data.reverse()
@@ -29,3 +35,6 @@ def prep_probabilities(data: np.array):
     data.insert(len(data) + 1, 1.0)
 
     return data
+
+if __name__ == '__main__':
+    cc.compile()
